891c6628fc2da0665453095107f6b0fb7ec42e93
amaliezzz <amirods05@gmail.com>
2025-02-28 23:19:04 -0500

    feat: pipe

---
+++ pipe.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import spacy
+import ast
+
+def preprocess_input(title: str):
+    
+    nlp = spacy.load('en_core_web_sm')
+    
+    return nlp(title).vector
+
+
+def cosine(a: np.array, b: np.array):
+    
+    return a.dot(b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+def find(title):
+    
+    vector = preprocess_input(title)
+    data = pd.read_csv('data/movies.csv')
+    
+    lst = []
+    
+    for i, row in data.iterrows():
+        vector_ = np.fromstring(row['vector'].strip('[]'), sep=' ')
+        
+        cos = cosine(vector_, vector)
+        if cos<0.99:
+            lst.append( [ cos, row['movie_title']]  )
+        
+    lst.sort(reverse=True)
+    
+    return lst[:10]
+        
+        
+        
+        
+    
